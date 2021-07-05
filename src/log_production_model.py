acf4007566eb7075dc2b05373d30115e9e261825
asokkalidass kalisamy <asok.kalidass@gmail.com>
2021-07-05 15:30:15 -0700

    Updated fetch logic - model logic

--- src/log_production_model.py
+++ src/log_production_model.py
@@ -7,6 +7,7 @@ import joblib
 import os
 import mlflow.pyfunc
 
+
 def log_production_model(config_path):
     config = read_params(config_path)
 
@@ -17,10 +18,14 @@ def log_production_model(config_path):
 
     mlflow.set_tracking_uri(remote_server_uri)
 
-    runs = mlflow.search_runs(experiment_ids = 1) #our exp id  is 1 under artifacts folder
+    # our exp id  is 1 under artifacts folder
+    runs = mlflow.search_runs(experiment_ids=1)
 
-    highest_silhouette_score = runs["metrics.silhouette_score"].sort_values(ascending=False)[0]
-    highest_run_id = runs[runs["metrics.silhouette_score"] == highest_silhouette_score]["run_id"][0]
+    highest_silhouette_score = runs["metrics.silhouette_score"].sort_values(
+        ascending=False).reset_index().iloc[0, 1]
+
+    highest_run_id = runs[runs["metrics.silhouette_score"]
+                          == highest_silhouette_score].iloc[0].run_id
 
     client = MlflowClient()
     for mv in client.search_model_versions(f"name='{model_name}'"):
@@ -29,7 +34,7 @@ def log_production_model(config_path):
         if mv["run_id"] == highest_run_id:
             current_version = mv["version"]
             logged_model = mv["source"]
-            pprint(mv, indent=4)
+            #pprint(mv, indent=4)
             client.transition_model_version_stage(
                 name=model_name,
                 version=current_version,
@@ -43,13 +48,12 @@ def log_production_model(config_path):
                 version=current_version,
                 stage="Staging"
             )
-    
+
     loaded_model = mlflow.pyfunc.load_model(logged_model)
     model_path = config["webapp_final_model_dir"]
 
     joblib.dump(loaded_model, model_path)
     joblib.dump(loaded_model, model_dir)
-     
 
 
 if __name__ == "__main__":
